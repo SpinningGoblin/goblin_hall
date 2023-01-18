73eee496747b028b4b1eb68bdeb643991df8a281
Derrick Plotsky <derrickplotsky@gmail.com>
2023-01-17 21:11:22 -0800

    Startup app

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use bevy::{
+    prelude::{App, ImagePlugin, PluginGroup, SystemSet},
+    DefaultPlugins,
+};
+use resources::sprites::Handles;
+use state::AppState;
+use systems::{startup, textures};
+
 mod resources;
 mod state;
 mod systems;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .init_resource::<Handles>()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_state(AppState::Startup)
+        .add_system_set(SystemSet::on_enter(AppState::Startup).with_system(textures::load))
+        .add_system_set(SystemSet::on_update(AppState::Startup).with_system(textures::check))
+        .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(startup))
+        .run();
 }
