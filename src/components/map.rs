e88c6680dc34c1ee63a14bb5ee5150e543f9365a
Derrick Plotsky <derrickplotsky@gmail.com>
2023-02-22 20:21:07 -0800

    Add method to map to check if coordinate walkable

--- src/components/map.rs
+++ src/components/map.rs
@@ -1,9 +1,19 @@
 use bevy::prelude::Component;
-use tdlg::map::TopDownMap;
+use tdlg::map::{cells::Coordinate, TopDownMap};
 
 #[derive(Component, Debug)]
 pub struct Map {
     pub current: TopDownMap,
     pub grid_size: u16,
     pub tile_size: f32,
+}
+
+impl Map {
+    pub fn is_coordinate_walkable(&self, coordinate: &Coordinate) -> bool {
+        self.current
+            .grid()
+            .cell(coordinate)
+            .map(|cell| cell.is_walkable())
+            .unwrap_or_default()
+    }
 }
