9644935748ebd0f6c2c0aa6f39401851ece0f309
Derrick Plotsky <derrickplotsky@gmail.com>
2023-02-25 20:34:13 -0800

    Give character a visibility box

--- src/components/characters/character.rs
+++ src/components/characters/character.rs
@@ -1,4 +1,20 @@
+use std::num::NonZeroI32;
+
 use bevy::prelude::Component;
+use tdlg::map::cells::Coordinate;
+
+use crate::components::GridBox;
 
 #[derive(Component)]
-pub struct Character;
+pub struct Character {
+    pub visibility: NonZeroI32,
+}
+
+impl Character {
+    pub fn visibility_box(&self, center: Coordinate) -> GridBox {
+        GridBox {
+            center,
+            size: self.visibility.get(),
+        }
+    }
+}
