a55d1203451f139bbc69bad40bab284a8ea72cfb
Derrick Plotsky <derrickplotsky@gmail.com>
2023-02-25 20:42:26 -0800

    Add system to show things in visible area

--- src/systems/characters/mod.rs
+++ src/systems/characters/mod.rs
@@ -1,1 +1,3 @@
+mod visible_area;
 
+pub use visible_area::show_in_visible_area;
---
+++ src/systems/characters/visible_area.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::{Query, Transform, Visibility, With};
+
+use crate::{
+    components::{characters::Character, structures::Body, GridBox, Map},
+    resources::config::grid::grid_coordinate_from_world,
+};
+
+pub fn show_in_visible_area(
+    query: Query<&Transform, With<Character>>,
+    mut visible_bodies: Query<(&mut Visibility, &Body)>,
+    map_query: Query<&Map>,
+) {
+    if map_query.is_empty() {
+        return;
+    }
+
+    let map = map_query.single();
+
+    let visibility_boxes: Vec<GridBox> = query
+        .iter()
+        .map(|transform| {
+            let coordinate = grid_coordinate_from_world(
+                &transform.translation.truncate(),
+                map.grid_size,
+                map.tile_size,
+            );
+            GridBox {
+                center: coordinate,
+                size: 10,
+            }
+        })
+        .collect();
+
+    for visibility_bundle in visible_bodies.iter_mut().filter(|(_, body)| {
+        visibility_boxes
+            .iter()
+            .any(|visibility_box| visibility_box.contains(&body.center_coordinate))
+    }) {
+        let (mut visibility, _) = visibility_bundle;
+        visibility.is_visible = true;
+    }
+}
