c7fc7a33921bb2d5ebb7490ef93f789878f5e179
Derrick Plotsky <derrickplotsky@gmail.com>
2023-04-08 22:40:21 -0700

    Add resource to spawnable

--- src/components/spawns/tdlg_spawnable.rs
+++ src/components/spawns/tdlg_spawnable.rs
@@ -1,10 +1,13 @@
 use bevy::prelude::Visibility;
 use tdlg::map::layers::LayerType;
 
+use crate::components::resources::Resource;
+
 use super::SpawnCoordinate;
 
 pub struct TdlgSpawnable {
     pub layer_type: LayerType,
+    pub resource: Option<Resource>,
     pub spawn_coordinate: SpawnCoordinate,
     pub visibility: Visibility,
 }
